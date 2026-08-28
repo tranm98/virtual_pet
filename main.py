0d2f79c574f79970d76ffb7d37cba330869aeaa9
Mai <114268221+tranm98@users.noreply.github.com>
2026-08-28 00:32:59 -0400

    updated player sleep mechanics

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame 
+import time
 print(pygame.version.ver)
 
 pygame.init()
@@ -12,22 +13,31 @@ class Player():
         self.current_energy = 75
         self.maximum_energy = 100
         self.energy_bar_length = 100
-        self.energy_ratio = self.maximum / self.energy_bar_length
+        self.energy_ratio = self.maximum_energy / self.energy_bar_length
     
     def update(self):
         pass
     
+    def get_time (self, amount):
+        if self.current_energy > 0:
+            self.current_energy -= amount
+        if self.current_energy <= 0:
+            self.current_energy = 0
+    
+    def get_sleep(self, amount):
+        if self.current_energy < self.maximum_energy:
+            self.current_energy += amount
+        if self.current_energy >= self.maximum_energy:
+            self.current_energy = self.maximum_energy
+
+    
 player = Player()
+
+sleeping=False
 #contributes to keeping the character from being too fast
 vel = 2
 
-def get_energy(self, amount):
-    if self.
-
-time = pygame.time.get_ticks()
-print(time)
-
-
+            
 bed = pygame.image.load("bed.png")
 bed = pygame.transform.scale(bed,(80,50))
 
@@ -42,6 +52,9 @@ while run:
             run = False
         if event.type == pygame.KEYDOWN:
             print(pygame.key.name(event.key))
+            if event.key == pygame.K_SPACE and player.rect.colliderect(bed_rect):
+                sleeping = True
+                  
     keys = pygame.key.get_pressed()
     #user movement
     player.rect.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * vel
@@ -55,11 +68,12 @@ while run:
     window.blit(bed, bed_rect)
     
     collide = player.rect.colliderect(bed_rect)
-    if collide:
-        print("hey, you've hit me!")
+    if collide and keys[pygame.K_SPACE]:
+            player.get_sleep(10)
+            print("mimimimimi, i'm sleeping")
     
     #the screen
-    pygame.draw.rect(window, (255, 0, 0), player)
+    pygame.draw.rect(window, (255, 0, 0), player.rect)
     
     pygame.display.flip()
     #contributes to keeping the character from being too fast
